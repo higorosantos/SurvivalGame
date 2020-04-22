53e5feba8c34bf631b97a61387901e46a4666e0e
ruanthow <ruan.thow3@gmail.com>
2020-04-21 23:46:58 -0300

    correção da animaaçõ

---
+++ Client/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Models;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerModel player;
+    [SerializeField]private Text name;
+    [SerializeField]private Text hp;
+    void Start()
+    {
+        player = new PlayerModel();
+        player.setName("Higor Oliveira");
+        player.setArmor(20);
+        name.text = player.getName();
+        hp.text = player.getHp().ToString();
+
+
+    }
+    private void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider obj)
+    {
+       //Destroy(obj.gameObject);
+        player.Damage(25);
+        hp.text = player.getHp().ToString();
+
+    }
+}
--- Client/Assets/Scripts/Model/Player.cs
+++ Client/Assets/Scripts/Model/Player.cs
@@ -6,18 +6,57 @@ namespace Models {
     public class PlayerModel
     {
         private string name;
-        private float hp;
+        private float hp = 100;
         private float armor;
+        private float stamina = 100;
+        private float xp = 0;
+        
 
+        #region Set's
         public void setName(string name)
         {
             this.name = name;
-
-
         }
+        public void setArmor(float armor)
+        {
+            this.armor = armor;
+        }
+        #endregion
+
+        #region Get's
         public string getName()
         {
             return this.name;
         }
-}
+        public float getHp()
+        {
+            return this.hp;
+        }
+        public float getArmor()
+        {
+            return this.armor;
+        }
+        public float getStamina()
+        {
+            return this.stamina;
+        }
+        public float getExp()
+        {
+            return this.xp;
+        }
+        #endregion
+
+        public void HealHP(float hp)
+        {
+            this.hp = this.hp + hp;
+        }
+        public void Damage(float damage)
+        {
+            this.hp = this.hp - damage;
+        }
+        public void addExp(float xp)
+        {
+            this.xp = this.xp + xp;
+        }
+    }
 }
